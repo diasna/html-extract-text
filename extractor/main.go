81092de0f278fc69351086e8a87010c50812711f
diasna <dias.arifin@gmail.com>
2022-01-19 21:16:21 +0700

    add some security

--- extractor/main.go
+++ extractor/main.go
@@ -8,10 +8,16 @@ import (
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		Body:       os.Getenv("API_KEY"),
-		StatusCode: 200,
-	}, nil
+	if request.Headers["X-API-Key"] == os.Getenv("API_KEY") {
+		return events.APIGatewayProxyResponse{
+			Body:       os.Getenv("API_KEY"),
+			StatusCode: 200,
+		}, nil
+	} else {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 401,
+		}, nil
+	}
 }
 
 func main() {
